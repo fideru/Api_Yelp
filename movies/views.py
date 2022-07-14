6780e9fd363afa04e093c147fa4ac507978448df
Fidel Baez <baezlopezfidel24@gmail.com>
2022-07-14 10:10:25 +0900

    "Commit/push Web Django Project and other library tests"

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 
 # Represents main page of any app
 def index(request):
-    return HttpResponse("What Upp Nyugga")
+    movies = Movie.objects.all()
+    return (render(request, 'index.html', {'movies': movies}))
+#    output = ','.join(m.title for m in movies)
